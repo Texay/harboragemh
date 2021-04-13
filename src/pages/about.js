5a0d3fe4ba1fa3bfffa5ca63e06679a1d450061c
Wesley <starclad1019@gmail.com>
2021-04-12 23:01:22 -0500

    v0.0.0.1

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: `blue` }}>
+      <Header headerText="About Harborage" />
+      <Header headerText="Testing" />
+      <p>Idk yet</p>
+    </div>
+  )
+}
